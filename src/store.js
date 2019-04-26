e7ff87f3459d05cbbdcea2674b7454ab12033644
michael mudge <michaeltmudge@gmail.com>
2019-04-26 20:08:33 +0200

    working on fetching jwt tokens

--- src/store.js
+++ src/store.js
@@ -1,23 +1,35 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import router from "@/router";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    appName: 'Pic Post'
+    appName: 'Pic Post',
+    user: null,
+    isAuthenticated: false
   },
-  mutations: {},
+  mutations: {
+    setUser(state, payload) {
+      state.user = payload;
+    },
+    setIsAuthenticated(state, payload) {
+      state.isAuthenticated = payload;
+    }
+  },
   actions: {
     userJoin({ commit }, { email, password, password_confirmation }) {
       return fetch("http://localhost:3000/signup", {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
+        // credentials: 'include',
         // mode: "cors", // no-cors, cors, *same-origin
         // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        // credentials: "same-origin", // include, *same-origin, omit
+        credentials: "same-origin", // include, *same-origin, omit
         headers: {
           "Accept": "application/json",
           "Content-Type": "application/json",
+          "Authorization": "JWT"
           // "Content-Type": "application/x-www-form-urlencoded",
         },
         // redirect: "follow", // manual, *follow, error
@@ -25,18 +37,45 @@ export default new Vuex.Store({
         // body: JSON.stringify(data), // body data type must match "Content-Type" header
         body: JSON.stringify( {
           user: {
-            email: 'mikey@gmail.com',
-            password: 'abc123',
-            password_confirmation: 'abc123'
+            email: email,
+            password: password,
+            password_confirmation: password_confirmation
           }
         })
       })
         .then((response) => {
-          response.json()
-          console.log(response)
+          response.json();
+          // console.log(response)
         });
+    },
+    userLogin({ commit }, { email, password }) {
+      const u = { user: { email: email, password: password }}
+      return fetch("http://localhost:3000/login", {
+        method: "POST",
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+          "Authorization": "JWT"
+          // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: JSON.stringify(u)
+      })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
 
-
+        console.log(response);
+        if (response.token) {
+          commit('setUser', { user: { email: response.email, token: response.token }});
+          commit('setIsAuthenticated', true);
+          router.push("dashboard");
+        }
+      })
+      .catch((error) => {
+        console.log('login didnt work', error)
+      })
     }
   }
 });
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,14 +7,13 @@
             <v-toolbar-title>Login</v-toolbar-title>
           </v-toolbar>
           <v-card-text>
-            <v-form ref="form" v-model="valid" lazy-validation>
+            <v-form ref="form" lazy-validation>
               <v-text-field
                 prepend-icon="person"
                 name="email"
                 label="Email"
                 type="email"
                 v-model="email"
-                :rules="emailRules"
                 required
               ></v-text-field>
               <v-text-field
@@ -25,13 +24,12 @@
                 type="password"
                 required
                 v-model="password"
-                :rules="passwordRules"
               ></v-text-field>
             </v-form>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary" :disabled="!valid" @click="submit">Login</v-btn>
+            <v-btn color="primary" @click="submit">Login</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
@@ -46,15 +44,7 @@ export default {
     return {
       valid: false,
       email: "",
-      password: "",
-      emailRules: [
-        v => !!v || "E-mail is required",
-        v => /.+@.+/.test(v) || "E-mail must be valid"
-      ],
-      passwordRules: [
-        v => !!v || "Password is required",
-        v => v.length >= 6 || "Password must be greater than 6 characters"
-      ]
+      password: ""
     };
   },
   methods: {
