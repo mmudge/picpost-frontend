f89c98c4910e43b48a812525db33a5d085f882f7
michael mudge <michaeltmudge@gmail.com>
2019-08-14 08:04:07 +0200

    finished sent and received tabs

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -17,8 +17,6 @@
             <li>edit user info</li>
             <li>profile page</li>
             <li>home page/ landing page design</li>
-
-            <li>sent / received tabs</li>
             <li>rspec back end test suite</li>
             <li>integrate active storage uploads from front end (somehow?) :\</li>
             <li>refactor some components and views because right now they dont make sense, postsIndex and posts, messages and inbox</li>
